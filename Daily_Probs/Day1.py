a97814158f4b5f769b3e741ae08d83f97ebbd41a
Sangeetha <sangeethapa2002@gmail.com>
2023-06-27 10:21:29 +0530

    Day1[Codechef Problem]

---
+++ Daily_Probs/Day1.py
@@ -0,0 +1,12 @@
+'''Write a program 
+to find the factorial value of any number entered by the user'''3
+
+
+# cook your dish here
+T=int(input("T:"))
+for i in range(T):
+    N=int(input("N:"))
+    s=1
+    for x in range(1,N+1):
+        s*=x
+    print(s)
