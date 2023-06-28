0c07e1b780ef921039eb5fedbb16b2f1cf5cb026
Sangeetha <sangeethapa2002@gmail.com>
2023-06-28 13:59:21 +0530

    Committed the message

---
+++ Daily_Probs/Day2.py/Prob1.py
@@ -0,0 +1,20 @@
+'''S -hiiden letter
+T-guess letter
+if the guess letter is correct print("G)
+else print("B)
+Ex:ABCDE
+BACED
+here the letter c is correct so print("G")
+the output will be : BBGBB'''
+
+# cook your dish here
+E=int(input("E"))
+for i in range(E):
+    S=str(input("S:"))
+    T=str(input("T:"))
+    for i in range(5):
+        if S[i]==T[i]:
+            print("G",end="")
+        else:
+            print("B",end="")
+    print("")
