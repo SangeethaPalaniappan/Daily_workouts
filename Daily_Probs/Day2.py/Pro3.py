3f997680f30135023e3cf61092a3d6307a112ada
Sangeetha <sangeethapa2002@gmail.com>
2023-06-29 17:50:06 +0530

    changes commited

---
+++ Daily_Probs/Day2.py/Pro3.py
@@ -0,0 +1,18 @@
+'''On breaking, the string 
+S gives A=abc and B=abc. Thus, joining it in either way
+(AB or BA), would give the same string S
+'''
+
+
+# cook your dish here
+T=int(input())
+for i in range(T):
+    N=int(input())
+    S=str(input())
+    e=S[0:N//2]
+    f=S[N//2:N]
+
+    if e==f:
+        print("YES")
+    else:
+        print("NO")
