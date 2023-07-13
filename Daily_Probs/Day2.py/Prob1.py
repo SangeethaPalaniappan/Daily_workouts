70aaf67b43d41dfeab9e1318793f40f0ea3667c6
Sangeetha <sangeethapa2002@gmail.com>
2023-07-13 15:25:30 +0530

    Use Cases

--- Daily_Probs/Day2.py/Prob1.py
+++ Daily_Probs/Day2.py/Prob1.py
@@ -6,17 +6,64 @@ Ex:ABCDE
 BACED
 here the letter c is correct so print("G")
 the output will be : BBGBB'''
-
+#need to work on use cases - worked 
 # cook your dish here
+
 E=int(input("E"))
 for i in range(E):
+    print("\n")
     S=str(input("S:"))
     T=str(input("T:"))
-    for i in range(5):
-        if S[i]==T[i]:
-            print("G",end="")
+  
+    m=""
+    for i in S:
+        s=ord(i)
+        
+        #If the character is uppercase and it need to be converted it into lower
+        if s>=65 and s<=90:
+            r=chr(s+32)
+            m=m+r
         else:
-            print("B",end="")
+            r=i
+            m=m+r
+    
+    S=m
+        #If the character is lowercase and it need to be converted it into upper
+        #if s>=97 and s<=122:
+         #   print(chr(s-32),end="")
+        #else:
+         #   print(i,end="")'''
 
-    print("")
-    print("")
+    n=""
+    for j in T:
+        sd=ord(j)
+        #If the character is uppercase and it need to be converted it into lower
+        if sd>=65 and sd<=90:
+            t=chr(sd+32)
+            n=n+t
+        else:
+            t=j
+            n=n+t
+    T=n        
+
+        #If the character is lowercase and it need to be converted it into upper    
+        #if s>=97 and s<=122:
+         #   print(chr(s-32),end="")
+        #else:
+         #   print(j,end="")'''
+    if len(S)==len(T):
+        for i in range(len(S)):  
+        
+            if S[i]==T[i]:
+                print("G",end="")
+            else:
+                print("B",end="")
+    
+        
+    else:
+        #print("Length of the strings are not equal")
+        for i in range(len(S)):# use the length of the original string
+            print("B",end="")
+    
+
+    
--- Daily_Probs/Day2.py/Prob2.py
+++ Daily_Probs/Day2.py/Prob2.py
@@ -1,17 +1,65 @@
 '''On breaking, the string 
+
 S gives A=abc and B=abc. Thus, joining it in either way
-(AB or BA), would give the same string S
-'''
-
+(AB or BA), would give the same string S'''
 # cook your dish here
-T=int(input())
+def string(N,S):
+    d=N//2   
+    m=len(S)
+    if N!=m:
+        print("Given length and the string lenght is not equal")
+    else:
+        if N==0:
+            print("No string is given")
+            return 0
+        m=""
+        for i in S:
+            s=ord(i)
+        
+        #If the character is in uppercase and it need to be converted it into lower
+            if s>=65 and s<=90:
+                r=chr(s+32)
+                m=m+r
+            else:
+                r=i
+                m=m+r
+        
+        #If the character is in lowercase and it need to be converted it into upper
+        #if s>=97 and s<=122:
+        #   r=chr(s-32)
+        #else:
+            #r=i
+            #m=m+r'''
+         
+        S=m
+        if N%2==0:
+            e=S[0:d]
+            f=S[d:N]
+            for x in range(d): 
+                if e[x]==f[x]:
+                    pass
+                else:
+                    return 0
+                    
+            return 1    
+        else:
+            e=S[0:d]
+            f=S[d+1:N]
+            for x in range(d): 
+                if e[x]==f[x]:
+                    pass
+                else:
+                    return 0
+                
+        return 1
+        
+            
+    
+T=int(input("T:"))
 for i in range(T):
-    N=int(input())
-    S=str(input())
-    e=S[0:N//2]
-    f=S[N//2:N]
-
-    if e==f:
+    N=int(input("N:"))
+    S=str(input("S:"))
+    if string(N,S)==1:
         print("YES")
     else:
         print("NO")
