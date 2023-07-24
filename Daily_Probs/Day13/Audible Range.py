ef1b495fa0e433858259d1549d14eb7a8c2fb9e9
Sangeetha <sangeethapa2002@gmail.com>
2023-07-24 10:19:34 +0530

    Problems added

---
+++ Daily_Probs/Day13/Audible Range.py
@@ -0,0 +1,8 @@
+# cook your dish here
+T=int(input("T:"))
+for x in range(T):
+    X=int(input("X:"))
+    if X>=67 and X<=45000:
+        print("YES")
+    else:
+        print("NO")
---
+++ Daily_Probs/Day13/To solve the sorting.py
@@ -0,0 +1,2 @@
+#Need to solve 
+#To solve the list in non-descending order
